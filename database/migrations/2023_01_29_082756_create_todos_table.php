63e2450269994362025965545fe7d37551805d02
Kishan <bang.siki2009@gmail.com>
2023-02-01 21:07:50 +0545

    Modified migrations

--- database/migrations/2023_01_29_082756_create_todos_table.php
+++ database/migrations/2023_01_29_082756_create_todos_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('todo');
             $table->timestamps();
         });
